5383a5aeabaed7f9cfe2685bb845ee0e7105656f
Carrington <hartis.c99@gmail.com>
2025-07-19 11:35:39 -0400

    media queries

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,7 +5,7 @@ export const Navbar = () => {
     return (
         <Link to="/">
             <div className="nav-container">
-                <h1>Tech by Nixia</h1>
+                <h1>{"<"} TechByNixia /{">"}</h1>
             </div>
         </Link>
     )
