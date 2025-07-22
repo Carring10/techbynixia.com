d5ae676e491df83cb0331393295a36e468343899
Carrington <hartis.c99@gmail.com>
2025-07-22 17:10:51 -0400

    homepage styling

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,11 +1,22 @@
 import { Navbar } from "../Navbar/Navbar";
-import { ArticleList } from "../ArticleList/ArticleList";
+// import { ArticleList } from "../ArticleList/ArticleList";
+import "./home.css";
 
 export const Home = () => {
     return (
         <>
             <Navbar />
-            <ArticleList />
+            <div className="home-container">
+                <div className="home-contents-container">
+                    <h1 className="heading">Tech made simple</h1>
+                    <p className="subheading">Because you don't need a degree to fix your PC</p>
+                    <div className="button-container">
+                        <button>Start Reading</button>
+                        <button>About Me</button>
+                    </div>
+                </div>
+                    <img className="logo" src="images/canva-logo.png"/>
+            </div>
         </>
     )
 }
