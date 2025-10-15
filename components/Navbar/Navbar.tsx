f23035e59f15b6e82f2c70a7e7a574ee805b0970
Carrington <hartis.c99@gmail.com>
2025-10-15 19:46:42 -0400

    styling

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,26 +1,32 @@
-import React from 'react';
-import Image from 'next/image';
+import { useEffect, useState } from "react";
+import Link from "next/link";
 import styles from "./Navbar.module.css";
-import Link from 'next/link';
 
 const Navbar = () => {
-    return (
-        <nav className={styles.nav}>
-            <Link href="/">
-                <Image
-                    src="/techbynixia-logo.png"
-                    width={150}
-                    height={70}
-                    alt="Laptop logo"
-                />
-            </Link>
-            <div className={styles.links}>
-                <h4>SHOP</h4>
-                <Link href="/articles"><h4>GUIDES</h4></Link>
-                <h4>ABOUT ME</h4>
-            </div>
-        </nav>
-    )
-}
+  const [scrolled, setScrolled] = useState(false);
 
-export default Navbar
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrolled(window.scrollY > 10); 
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <nav className={`${styles.nav} ${scrolled ? styles.scrolled : ""}`}>
+      <Link href="/" className={styles.brand}>
+        <h2>Tech by Nixia</h2>
+      </Link>
+
+      <div className={styles.links}>
+        <h4>SHOP</h4>
+        <Link href="/articles"><h4>GUIDES</h4></Link>
+        <h4>ABOUT ME</h4>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,25 @@
+import Link from "next/link";
 import styles from "../page.module.css";
 
 export default function Home() {
   return (
     <>
       <main className={styles.main}>
-        <h1>Today's Hottest Deals</h1>
+        {/* <h1 className={styles.featured}>Today's Hottest Deals</h1> */}
         <div className={styles.content}></div>
+        <h2>Recent Posts</h2>
+        <div className={styles.postsContainer}>
+          <div>
+            <div className={styles.recentPosts}></div>
+            <h2>This is an example of a title</h2>
+            <Link className={styles.link} href="">Read More</Link>
+          </div>
+          <div>
+            <div className={styles.recentPosts}></div>
+            <h2>This is an example of a title</h2>
+            <Link className={styles.link} href="">Read More</Link>
+          </div>
+        </div>
       </main>
     </>
   );
